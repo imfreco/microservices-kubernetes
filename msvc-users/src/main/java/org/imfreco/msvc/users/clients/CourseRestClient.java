547df7d3d0481b209c477e2939b158edf8a1fc3e
freco <fred.cor.14@gmail.com>
2025-02-10 03:14:27 -0500

    dockerfile created to msvc-users

--- msvc-users/src/main/java/org/imfreco/msvc/users/clients/CourseRestClient.java
+++ msvc-users/src/main/java/org/imfreco/msvc/users/clients/CourseRestClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-courses", url = "localhost:8002/courses")
+@FeignClient(name = "msvc-courses", url = "host.docker.internal:8002/courses")
 public interface CourseRestClient {
     @DeleteMapping("/course-user/{userId}")
     void deleteCourseUserByUserId(@PathVariable Long userId);
