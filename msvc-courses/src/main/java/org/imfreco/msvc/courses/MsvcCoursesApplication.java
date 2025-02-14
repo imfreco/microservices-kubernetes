fb9473ea4d7ccce69696b4f961971f1661f49b02
freco <fred.cor.14@gmail.com>
2025-02-14 16:57:20 -0500

    rename compose and use dotenv

--- msvc-courses/src/main/java/org/imfreco/msvc/courses/MsvcCoursesApplication.java
+++ msvc-courses/src/main/java/org/imfreco/msvc/courses/MsvcCoursesApplication.java
@@ -1,14 +1,25 @@
 package org.imfreco.msvc.courses;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.util.Objects;
 
 @SpringBootApplication
 @EnableFeignClients
 public class MsvcCoursesApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().directory("./msvc-courses/").load();
+        System.setProperty("PORT", Objects.requireNonNull(dotenv.get("PORT")));
+        System.setProperty("DB_HOST", Objects.requireNonNull(dotenv.get("DB_HOST")));
+        System.setProperty("DB_NAME", Objects.requireNonNull(dotenv.get("DB_NAME")));
+        System.setProperty("DB_USER", Objects.requireNonNull(dotenv.get("DB_USER")));
+        System.setProperty("DB_PASS", Objects.requireNonNull(dotenv.get("DB_PASS")));
+        System.setProperty("USERS_CLIENT_URL", Objects.requireNonNull(dotenv.get("USERS_CLIENT_URL")));
+
         SpringApplication.run(MsvcCoursesApplication.class, args);
     }
 
--- msvc-users/src/main/java/org/imfreco/msvc/users/MsvcUsersApplication.java
+++ msvc-users/src/main/java/org/imfreco/msvc/users/MsvcUsersApplication.java
@@ -1,14 +1,25 @@
 package org.imfreco.msvc.users;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.util.Objects;
 
 @SpringBootApplication
 @EnableFeignClients
 public class MsvcUsersApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().directory("./msvc-users/").load();
+        System.setProperty("PORT", Objects.requireNonNull(dotenv.get("PORT")));
+        System.setProperty("DB_HOST", Objects.requireNonNull(dotenv.get("DB_HOST")));
+        System.setProperty("DB_NAME", Objects.requireNonNull(dotenv.get("DB_NAME")));
+        System.setProperty("DB_USER", Objects.requireNonNull(dotenv.get("DB_USER")));
+        System.setProperty("DB_PASS", Objects.requireNonNull(dotenv.get("DB_PASS")));
+        System.setProperty("COURSES_CLIENT_URL", Objects.requireNonNull(dotenv.get("COURSES_CLIENT_URL")));
+
         SpringApplication.run(MsvcUsersApplication.class, args);
     }
 
